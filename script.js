7b48b135c4fcca2fe79d116966342327c4989a54
JesusAlvarez <yisus.corso@gmail.com>
2022-07-12 20:06:38 +0200

    Incorporación de array (y mejora en código anterior

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 
 
-function nombreAlumno(nombre) {
-    //"Hola, me llamo " + nombre + " y te estoy saludando"
-    return `Hola ${nombre}! la suma de tus notas es:`
+// function nombreAlumno(nombre) {
+//     //"Hola, me llamo " + nombre + " y te estoy saludando"
+//     return `Hola ${nombre}! la suma de tus notas es:`
+// }
+// console.log(nombreAlumno("Marcos"))
+
+nombre = prompt("Introduce tu nombre")
+
+let nota1, nota2, nota3, nota4;
+let dato1, dato2, dato3, dato4, media;
+dato1 = window.prompt("Ingresa la nota de ingles");
+nota1 = parseInt(dato1);
+dato2 = window.prompt("Ingresa la nota de matemáticas");
+nota2 = parseInt(dato2);
+dato3 = window.prompt("Ingresa la nota de latin");
+nota3 = parseInt(dato3);
+dato4 = window.prompt("Ingresa la nota de historia del arte");
+nota4 = parseInt(dato4);
+
+media = (nota1 + nota2 + nota3 + nota4)/3;
+
+if (media <5) {
+    resultat = "Suspendido"
+} else {
+    resultat = "Aprobado. ¡Felicidades!"
 }
-console.log(nombreAlumno("Marcos"))
+// document.write(`La nota final es ${resultat}`)
+console.log(`${nombre}, la valoración sobre la media de tus notas es: ${resultat}`)
 
-function sumar(numero1, numero2, numero3, numero4) {
-    console.log(numero1 + numero2 + numero3 + numero4);
-}
-
-let numero1 = parseFloat(prompt("Ingresa la nota de ingles"));
-let numero2 = parseFloat(prompt("Ingresa la nota de matemáticas"));
-let numero3 = parseFloat(prompt("Ingresa la nota de latin"));
-let numero4 = parseFloat(prompt("Ingresa la nota de historia del arte"));
-sumar(numero1, numero2, numero3, numero4);
-
-// aqui está el bloqueo. Mi lógica seria: Crear una función que me diese un resultado por lo que diría:
-// Vamos a crear una función que contenga el resultado de la funcion "suma" y que a su vez esté
-// dividido entre número de asignaturas. Todo esto es lo que no sé como traducir en código
-// Necesito mucha ayuda :(
+const materias = ["Ingles", "Matemáticas", "Latin", "Hisotria del arte",]
+console.log(materias)
