03f683f78323d0106e4720b0891d94457902ca09
JesusAlvarez <yisus.corso@gmail.com>
2022-07-14 17:49:49 +0200

    actualización del array

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-
-// function nombreAlumno(nombre) {
-//     //"Hola, me llamo " + nombre + " y te estoy saludando"
-//     return `Hola ${nombre}! la suma de tus notas es:`
-// }
-// console.log(nombreAlumno("Marcos"))
-
 nombre = prompt("Introduce tu nombre")
 
 let nota1, nota2, nota3, nota4;
@@ -26,8 +19,25 @@ if (media <5) {
 } else {
     resultat = "Aprobado. ¡Felicidades!"
 }
-// document.write(`La nota final es ${resultat}`)
+
 console.log(`${nombre}, la valoración sobre la media de tus notas es: ${resultat}`)
 
-const materias = ["Ingles", "Matemáticas", "Latin", "Hisotria del arte",]
+
+class Materia {
+    constructor(id, nombre, duracion, numeroEvaluaciones, examenes){
+        this.id = id
+        this.nombre = nombre
+        this.duracion = duracion
+        this.numeroEvaluaciones = numeroEvaluaciones
+        this.examenes = examenes
+    }
+}
+
+const materia1 = new Materia(1, "Ingles", "3 trimestres", 3, ["examen 1", "examen 2", "examen 3"])
+const materia2 = new Materia(2, "Matemáticas", "3 trimestres", 3, ["examen 1", "examen 2", "examen 3"])
+const materia3 = new Materia(3, "Latín", "Marron", "2 trimestres", 2, ["examen 1", "examen 2"])
+const materia4 = new Materia(4, "Hisotria del Arte", "1 trimestre", 1, ["examen 1"])
+
+const materias = [materia1, materia2, materia3, materia4]
+
 console.log(materias)
